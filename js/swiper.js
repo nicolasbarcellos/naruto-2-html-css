e7cf195d1938f91f557d94a0f76e3e482225cc9a
Nicolas <nicolasbarcellos95@gmail.com>
2021-08-10 02:35:38 -0300

    ada

--- js/swiper.js
+++ js/swiper.js
@@ -43,39 +43,6 @@ const swiper = new Swiper('.swiper-container-cardCla', {
 
 
 
-JavaScript
-const $responsiveCarousel = document.querySelector(".js-carousel--responsive");
-
-new Glider($responsiveCarousel, {
-  slidesToShow: 1,
-  slidesToScroll: 1,
-  draggable: true,
-  dots: ".js-carousel--responsive-dots",
-  arrows: {
-    prev: ".js-carousel--responsive-prev",
-    next: ".js-carousel--responsive-next",
-  },
-  responsive: [
-    {
-      breakpoint: 600,
-      settings: {
-        slidesToShow: 2,
-        slidesToScroll: 2,
-      },
-    },
-    {
-      breakpoint: 900,
-      settings: {
-        slidesToShow: 3,
-        slidesToScroll: 3,
-      },
-    },
-  ],
-});
-
-
-
-
 const swiper1 = new Swiper('.swiper-container-cardPoints', {
   // Optional parameters
   direction: 'horizontal',
