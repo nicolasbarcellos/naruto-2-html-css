493f870fa2c8ad9645dea74c869f66a9478c4bad
Nicolas <nicolasbarcellos95@gmail.com>
2021-08-10 02:29:55 -0300

    a

---
+++ js/swiper.js
@@ -0,0 +1,144 @@
+const swiper = new Swiper('.swiper-container-cardCla', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: false,
+  breakpoints: {
+    // when window width is >= 640px
+    920: {
+      slidesPerView: 5,
+      spaceBetween: 0
+    },
+    500: {
+      slidesPerView: 3,
+      spaceBetween: 0
+    },
+    384: {
+      slidesPerView: 2,
+      spaceBetween: 0,
+    },
+    300: {
+      slidesPerView: 2,
+      spaceBetween: 0
+    }
+  },
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
+
+
+
+
+
+JavaScript
+const $responsiveCarousel = document.querySelector(".js-carousel--responsive");
+
+new Glider($responsiveCarousel, {
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  draggable: true,
+  dots: ".js-carousel--responsive-dots",
+  arrows: {
+    prev: ".js-carousel--responsive-prev",
+    next: ".js-carousel--responsive-next",
+  },
+  responsive: [
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+      },
+    },
+    {
+      breakpoint: 900,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+      },
+    },
+  ],
+});
+
+
+
+
+const swiper1 = new Swiper('.swiper-container-cardPoints', {
+  // Optional parameters
+  direction: 'horizontal',
+  slidesPerView: 4,
+  loop: false,
+
+  breakpoints: {
+    // when window width is >= 640px
+    920: {
+      slidesPerView: 4,
+      spaceBetween: 0
+    },
+    500: {
+      slidesPerView: 3,
+      spaceBetween: 0
+    },
+    384: {
+      slidesPerView: 2,
+      spaceBetween: 0,
+    },
+    300: {
+      slidesPerView: 2,
+      spaceBetween: 0
+    }
+  },
+  
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
+
+const hamburguer = document.querySelector('.hamburguer');
+const panel = document.querySelector('.panel');
+const btnClose = document.querySelector('.mobile-close');
+const menuFixedClose = document.querySelector('.menu-fixed-close');
+const menuFixedContent = document.querySelector('.menu-fixed-wrapper');
+const menuFixed = document.querySelector('.menu-fixed');
+
+menuFixed.addEventListener('click', () => {
+  menuFixedContent.classList.add('active');
+})
+
+menuFixedClose.addEventListener('click', () => {
+  menuFixedContent.classList.remove('active')
+})
+
+hamburguer.addEventListener('click', () => {
+  panel.classList.add('active');
+  menuFixedContent.classList.remove('active')
+})
+
+btnClose.addEventListener('click', () => {
+  panel.classList.remove('active');
+})
